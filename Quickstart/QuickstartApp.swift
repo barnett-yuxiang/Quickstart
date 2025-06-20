7fad426cd29c3546048e29c0dad9ff4706cbd837
yuli.kamakura <barnett.yuxiang@gmail.com>
2025-06-20 19:56:46 +0800

    Initial commit

---
+++ Quickstart/QuickstartApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickstartApp.swift
+//  Quickstart
+//
+//  Created by yuxiang on 2025/6/19.
+//
+
+import SwiftUI
+
+@main
+struct QuickstartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuickstartTests/QuickstartTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuickstartTests.swift
+//  QuickstartTests
+//
+//  Created by yuxiang on 2025/6/19.
+//
+
+import Testing
+@testable import Quickstart
+
+struct QuickstartTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
